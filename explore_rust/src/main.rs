ce4579a346cd93017303d9db39ced861c6792aa2
taf <dev@taf.codes>
2022-02-03 14:57:09 -0500

    clean stage

--- explore_rust/src/main.rs
+++ explore_rust/src/main.rs
@@ -1,25 +1,3 @@
 fn main() {
-    //variable shadowing time.
-
-    let x = 5;
-
-    let x = x + 1;
-
-    { //apparently I can just have a block anywhere to separate scope
-        let x = x * 2;
-        println!("Value of X in inner scope is : {}", x);
-    }
-
-    println!("Value of X in outer scope is {}", x);
 
 }
-
-
-//ok, so what?
-//shadows can be different types.  This is different than mutation,
-//it's just the name being reused.  
-
-//This is kind of nice, because in a function it means you can keep using
-//a name that you like, but then when execution leaves that scope you 
-//are assured that, if it wasn't marked 'mut', it didn't change.
-
