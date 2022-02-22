fb978c93971c78af8d47cc3577f1253da06de28d
taf <dev@taf.codes>
2022-02-21 20:52:46 -0500

    sigh, a brick wall is here until I learn how lifetimes work

--- structs_and_enums/src/main.rs
+++ structs_and_enums/src/main.rs
@@ -277,29 +277,33 @@ fn main() {
         Quarter(Option<UsState>)
     }
 
-    struct ChangePurse {
+    struct ChangePurse<> {
+        //coinpurse to which coins can be added but never removed
+        //and if you collect a quarter from all 4 states, you'll get 
+        //a notification when you do, and upon future status queries.
         owner: String,
         contents: Vec<Coin>,
         completed_state_quarter_collection: bool
     }
 
-    impl ChangePurse {
-        fn create(owner: String) -> ChangePurse {
+    impl ChangePurse<> {
+
+        fn create(owner: String) -> ChangePurse<> {
             ChangePurse {
                 owner,
-                Contents: Vec::new(), //type will be inferred????
+                contents: Vec::new(), //type will be inferred????
                 completed_state_quarter_collection: false
             }
         }
 
         fn add_coin(&self, c: Coin) {
+            //let my_contents=&self.contents;
             self.contents.push(c);
-            
         }
 
         fn value_in_cents(&self) -> i32 {
             let mut accum = 0;    
-            for coin in self.contents {
+            for coin in &self.contents {
                 accum += match coin {
                     Coin::Penny => 1,
                     Coin::Nickel => 5,
@@ -309,18 +313,24 @@ fn main() {
             }
             accum
         }
+
+        fn status(&self) {
+            println!("{}'s purse has {} cents",
+             self.owner,
+             self.value_in_cents());
+            if self.completed_state_quarter_collection {
+                println!("Congratulations, you've collected quarters from all 4 states");
+            } else {
+                println!("Keep trying to find those state quarters!")
+            }
+        }
     }
 
-    fn value_of_changepurse(cp: &ChangePurse) -> i32 {
-        
-    }
-
-    let my_currency = ChangePurse {
-        owner: String::from("taf"),
-        contents: vec![Coin::Penny, Coin::Quarter(None), Coin::Dime]
-    };
-    println!("{} 's purse has {} cents", my_currency.owner,
-     value_of_changepurse(&my_currency));
+    let mut my_currency = ChangePurse::create(String::from("taf"));
+    my_currency.add_coin(Coin::Penny);
+    my_currency.add_coin(Coin::Quarter(None));
+    my_currency.add_coin(Coin::Dime);
+    my_currency.status()
 
 }
 
